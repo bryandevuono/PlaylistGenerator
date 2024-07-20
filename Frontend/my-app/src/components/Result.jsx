2e1377e2a7b54d3a4255ac004fb8a4d034fd4eb4
bryandevuono <114662794+bryandevuono@users.noreply.github.com>
2024-07-20 12:51:31 +0200

    preview available and second hook added

--- Frontend/my-app/src/components/Result.jsx
+++ Frontend/my-app/src/components/Result.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import {useState} from 'react';
 import '../index.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation} from 'react-router-dom';
+
 const Result = () =>{
+  const [preview, setPreview] = useState(false);
+
+  const showPreview = (ref) => {
+    if(!preview){
+    setTimeout(() => {
+      setPreview(true)
+    }, 1000);
+    }
+    else{
+      setPreview(false)
+    }
+  }
   const location = useLocation();
   const state = location.state || {};
   const url = state.link;
   console.log(url);  
   const playlistId = url.split('/').pop().split('?')[0];
-    console.log(playlistId);
-  window.location.reload();
-
+  console.log(playlistId);
   return (
-    <div>
-      <div className='preview'>
+    <div className='homepage'>
+      <br/><br/>
+      <div className='resulttext'>
+        <p>Here is your Playlist!<br/>You can copy the link to access it:</p>
+        <p>{url}</p>
+      </div>
+
+      {preview ?<div className='preview'>
       <h2>Spotify Playlist Preview</h2>
       <iframe
         src={`https://open.spotify.com/embed/playlist/${playlistId}`}
         width="300"
         height="380"
-        allowTransparency="true"
         allow="encrypted-media"
         title="Spotify Playlist"
       ></iframe>
-      </div>
+      </div>: null}
+
+      <button className='previewbutton' onClick={showPreview}>Show preview</button>
     </div>
   );
 };
-
 export default Result;
---
+++ Frontend/my-app/src/hooks/useAPIGenre.jsx
@@ -0,0 +1,30 @@
+import {useNavigate} from 'react-router-dom';
+
+const useAPIGenre = ({genre}) =>{
+    const navigate = useNavigate()
+    const Fetch = (genre) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text: genre })
+        };
+        console.log(requestOptions)
+        try{
+            fetch('http://127.0.0.1:5000/api/genre', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.result);
+                    let link = data.result;
+                    console.log(link);
+                    navigate("/result", {state: {link}});
+                });
+        }catch (error){
+            navigate('/')
+            alert("something went wrong")
+            console.error("Error:", error)
+        }
+    }
+    return Fetch
+}
+
+export default useAPICustom
