5d15f47d5d06917ae73670a78e320aa03cde6a69
bryandevuono <bryandevuono@gmail.com>
2025-04-22 11:31:46 +0200

    New css

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -26,38 +26,53 @@ function App() {
 }
 function Home() {
   return (
-    <div className='homepage'>
-      <h1 className='homepagetext'>Playlist Generator</h1>
-      
-      <div className='text'>
-      <p>
-        Unlock Your Perfect Soundtrack!
-        <br/>
-        <br/>
-        Introducing Playlist Genie, your ultimate music companion for crafting the perfect playlist! <br/>Whether you’re looking to set the mood for a party, chill out after a long day, or <br/>discover new favorites, Playlist Genie makes it easy to generate personalized playlists tailored to you.
-        <br/><br/>
-        Make a playlist based on:
-      </p>
-      </div>
-      <br/><br/>
+     <div className='homepage'>
+        <div style={{display: "flex", flexDirection: "row"}}>
+           <div className='homepage-top'>
+              <h1 style={{display: "flex", flexDirection: "column"}} className='homepagetext'>Playlist Generator</h1>
+              <p>
+                 Unlock Your Perfect Soundtrack!
+                 <br/>
+                 <br/>
+                 Introducing Playlist Genie, your ultimate music companion for crafting the perfect playlist! <br/>Whether
+                 you’re looking to set the mood for a party, chill out after a long day, or <br/>discover new favorites,
+                 Playlist Genie makes it easy to generate personalized playlists tailored to you.
+                 <br/><br/>
+                 Make a playlist based on:
+              </p>
+              <div className='buttons'>
+                 <Link to='/genre'>
+                    <button style={{width: "300px"}} className='btn'>Genre</button>
+                 </Link>
+                 <br/><br/>
+                 <Link to='/mood'>
+                    <button style={{width: "300px"}} className='btn'>Mood</button>
+                 </Link>
+                 <br/><br/>
+                 <Link to='/custom'>
+                    <button style={{width: "300px"}} className='btn'>Custom</button>
+                 </Link>
+              </div>
+           </div>
+           <img className="homepageImg" alt="" src={homepageImage}></img>
+        </div>
 
-      <div className='buttons'>
-      <Link to ='/genre'><button className='btn'>Genre</button></Link>
-      <br/><br/>
-      <Link to='/mood'><button className='btn'>Mood</button></Link>
-      <br/><br/>
-      <Link to = '/custom'><button className='btn'>Custom</button></Link>
-      </div>
-      <img className="homepageImg" alt="" src={homepageImage}></img>
-      <br/>
-      
-      <p className='description-custom'>The custom query function is designed to create<br/> personalized playlists based on a user's specific preferences. <br/>This function accepts a range of input parameters,<br/> including music genres, mood, tempo, era, <br/>and artist preferences, to generate a curated <br/> playlist that aligns with the user's unique taste</p>
-      
-      <img src={homepageImage2} className="homepageImg2" alt=''></img>
-      <div className='get-started'>
-        <Link to = '/custom'><button className='btn'>Get Started</button></Link>
-      </div>
-    </div>
+        <div style={{display: "flex", flexDirection: "row", paddingTop: "20px", gap: "250px"}}>
+           <img src={homepageImage2} className="homepageImg2" alt=''></img>
+           <div style={{display: "flex", flexDirection: "column", paddingTop: "20px"}}>
+              <p className='description-custom'>The custom query function is designed to create<br/>
+                 personalized playlists based on a user's specific preferences. <br/>
+                 This function accepts a range of input parameters,<br/>
+                 including music genres, mood, tempo, era, <br/>
+                 and artist preferences, to generate a curated <br/>
+                 playlist that aligns with the user's unique taste
+              </p>
+              <Link to='/custom'>
+                 <button style={{width: "300px"}} className='btn'>Get Started</button>
+              </Link>
+           </div>
+        </div>
+     </div>
   );
 }
 
